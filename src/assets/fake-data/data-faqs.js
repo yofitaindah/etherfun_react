2db2c7213debe04d13e9b4a79ee4b860779dc40f
Yofita Indah <yofitaindah@gmail.com>
2024-12-09 08:52:37 +0700

    home-change17

--- src/assets/fake-data/data-faqs.js
+++ src/assets/fake-data/data-faqs.js
@@ -4,17 +4,17 @@ const dataFaq = [
     {
         id: 1,
         title: 'What is Etherfun?',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu'
+        content: 'EtherFun is a platform that allows users to create their own tokens on the Ethereum network, therfun is designed to make it easy to create tokens with low costs and high transparency through an efficient bonding curve model.'
     }, 
     {
         id: 2,
         title: 'How to access Etherfun?',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu'
+        content: 'To access the Etherfun platform, you can visit the official website at ether-fun.com.'
     },
     {
         id: 3,
         title: 'How do you make purchases in Etherfun?',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus eleifend egestas. Duis eget dictum purus. Fusce est mi, imperdiet porta neque malesuada, blandit scelerisque lectus. Maecenas vel lobortis risus. Donec ornare urna quis libero euismod, ac fermentum diam blandit. Nunc dapibus tempor lacus, vel lacinia enim efficitur eu'
+        content: 'To buy EtherFun tokens, install a wallet like Metamask, add the Ethereum network, and fill it with ETH to buy tokens and pay gas fees. Look for EtherFuns official contract address on the official site or social media, then open Uniswap and connect your Metamask. In Uniswap, select “Swap,” enter the EtherFun contract address, select the amount of ETH you want to exchange, then confirm the transaction in Metamask. Once done, manually add the tokens to Metamask via the “Import Tokens” option using the EtherFun contract address.'
     },
     // {
     //     id: 4,
